0394ba5398115414c6c2a7fada0fd14972f5b59b
Dmitry <joydnby@gmail.com>
2019-02-16 16:40:20 +0300

    k

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const API_KEY = process.env['var API_KEY'];
 console.log(API_KEY)
+console.log(process.env['var API_KEY'])console.log(process.env)
 const fetch = require('node-fetch');
 const path = require('path');
 const express = require('express');
